ff2868bb0ba139ec16da455851657a9fdcad6471
GhiasAli <ghiasali17@gmail.com>
2022-06-19 11:42:00 +0500

    final just before payment implementation

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -5,7 +5,7 @@ import app from '../firebase'
 import {useNavigate} from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 import { useSelector, useDispatch } from 'react-redux'
-import setLogedinEmail, {setKey} from "../Redux/actions";
+import setLogedinEmail, {setAlumniSchoolName, setKey} from "../Redux/actions";
 const db = getDatabase(app);
 function Login(props) {
 
@@ -112,10 +112,18 @@ console.log(`data ${key}`)
                         if(userType == 'Alumni')
                         {
                             if(approveCheck){
+                                dispatch(setAlumniSchoolName(SchoolName))
                                 navigate('/alumnilogin');
                             }
                             else{
                                 console.log('Sorry!, you are not approved')
+                                toast.custom(
+                                    <div style={{ marginTop: '5%',width: '100%', height: '6vh',  display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+
+                                        <div style={{ alignSelf: 'flex-start', width: '30%', height: '100%', borderLeftWidth: '8px', borderColor: 'red', borderStyle: 'solid', borderBottomWidth: 0, borderRightWidth: 0, borderTopWidth: 0, borderRadius: 5, backgroundColor: '#F5F5F5', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                            <h3 style={{ color: '#515C6F', fontFamily: 'GraphikMedium', fontWeight: '100', fontSize: '12px' }}>You are not approved</h3>
+                                        </div>
+                                    </div>, { duration: 1000 })
                             }
                         }
                         else {
@@ -131,10 +139,18 @@ console.log(`data ${key}`)
                         if(userType == 'Alumni')
                         {
                             if(approveCheck){
+                                dispatch(setAlumniSchoolName(SchoolName))
                                 navigate('/alumnilogin');
                             }
                             else{
                                 console.log('Sorry!, you are not approved')
+                                toast.custom(
+                                    <div style={{ marginTop: '5%',width: '100%', height: '6vh',  display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+
+                                        <div style={{ alignSelf: 'flex-start', width: '30%', height: '100%', borderLeftWidth: '8px', borderColor: 'red', borderStyle: 'solid', borderBottomWidth: 0, borderRightWidth: 0, borderTopWidth: 0, borderRadius: 5, backgroundColor: '#F5F5F5', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                            <h3 style={{ color: '#515C6F', fontFamily: 'GraphikMedium', fontWeight: '100', fontSize: '12px' }}>You are not approved</h3>
+                                        </div>
+                                    </div>, { duration: 1000 })
                             }
                         }
                         else {
